f054612af9673f6dfd95d6b4553ab97ee948e857
jjcblanco <jjcblanco@gmail.com>
2025-04-10 20:18:39 -0300

    mensaje

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import socket
 
+print("Este programa es el priemro en python")
 print("IP address:", socket.gethostbyname(socket.gethostname()))
 print("Hostname:", socket.gethostname())
 
 print("IP address:", socket.gethostbyname("www.google.com"))
 print("Hostname:", socket.gethostname())
+
 
